d18b4dc381c9d68e3260ce08d7474938364d43f9
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2023-11-24 14:41:36 +0700

    update start and join session functions

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { useNavigate } from "react-router-dom";
 import './App.css'
+import axios from 'axios';
 
 
 const session = {
@@ -22,18 +23,36 @@ function App() {
 
   const startSession = async () => {
     // Set URL
+    let lastInputUrl = inputUrl;
+    setInputUrl("");
     //  Send update request to backend
     // PATCH({sID: '123', status: 'ongoing', liveShareUrl: inputUrl})
+    const response = await axios.patch('http://localhost:3000/startSession', {
+      sID: '123', 
+      status: 'ongoing', 
+      liveShareUrl: lastInputUrl});
+    console.log(response);
+   
   }
 
 
   // STUDENT
 
-  const joinSession = () => {
+  const joinSession = async () => {
     // Only enable when time comes
     // request session data from backend
+    
     // GET({sID: '123'})
+    const response = await axios.get('http://localhost:3000/getSession', {
+      params: 
+      {sID: '123'}
+    });
+    console.log(response);
+    
     // If no URL yet then throw Error
+    if (!response.data.liveShareUrl) {
+      throw new Error('No URL yet');
+    }
   }
   
   return (
