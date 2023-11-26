e1bc4d5cc3aaf1a359a50c23b0a8c664c871ec60
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2023-11-27 02:17:31 +0700

    Not completed fe
    will update asap

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import { useNavigate } from "react-router-dom";
 import './App.css'
 import axios from 'axios';
+import logo from './images/address.png'
 
 const mySessionID = "123" // This is a fake ID, will have to implement scheduling feature
 
@@ -12,6 +13,9 @@ function App() {
   // const navigate = useNavigate();
   
   const [inputUrl, setInputUrl] = useState("");
+  const [renderComponent, setRenderComponent] = useState(null);
+
+  
 
   const handleChange = (event) => {
     setInputUrl(event.target.value)
@@ -34,10 +38,58 @@ function App() {
    
   }
 
+  // TUTOR
+  const tutorInterface = async () => {
+    setRenderComponent(
+      <>
+      <div className='headTitle'>
+        <h1>Meeting 68757</h1>
+        <div className='student'>
+          <img className='logo' src={logo} alt='student'></img>
+          <p>Student</p>
+        </div>
+        <div className='tutor'>
+          <img className='logo' src={logo} alt='tutor' onClick={tutorInterface}></img>
+          <p>Tutor</p>
+        </div>
+      </div>
+     
+  
+      <div className='infoLine date'>
+        <h3 className='title'>Date: </h3>
+        <h3 className='text'>Mon 09/11/2924</h3>
+      </div>
+  
+      <div className='infoLine startTime'>
+        <h3 className='title'>Start Time: </h3>
+        <h3 className='text'>12am PCT</h3>
+      </div>
+  
+      <div className='infoLine endTime'>
+        <h3 className='title'>End Time: </h3>
+        <h3 className='text'>2am PCT</h3>
+      </div>
+  
+      <h4 id='message'>To start the meeting, enter Live Share URL</h4>
+      <div className='startSession'>
+        <input type="text" placeholder='Live Share URL' onChange={handleChange}/>
+        <button onClick={startSession}>Start Session</button>
+      </div>
+      {/* <h1>Tutor's View</h1>
+      <input type="text" placeholder='Live Share URL' onChange={handleChange}/>
+      <button onClick={startSession}>Start Session</button>
+      <p>Live Share URL: {inputUrl}</p>
+      
+      <h1>Student's View</h1>
+      <button onClick={joinSession}>Join Session</button> */}
+      </>
+      )
+    }
+
+  
 
   // STUDENT
-
-  const joinSession = async () => {
+  const studentInterface = async () => {
     // Only enable when time comes
     // request session data from backend
     
@@ -49,19 +101,87 @@ function App() {
     if (!response.data.liveShareUrl) {
       throw new Error('No URL yet');
     }
-  }
-  
-  return (
-    <>
-    <h1>Tutor's View</h1>
-    <input type="text" placeholder='Live Share URL' onChange={handleChange}/>
-    <button onClick={startSession}>Start Session</button>
-    <p>Live Share URL: {inputUrl}</p>
+
+    if (response.data.status === 'ongoing') {
+      // Redirect to live share URL
+      setRenderComponent(<>
+        <div className='headTitle'>
+          <h1>Meeting 68757</h1>
+          <div className='student'>
+            <img className='logo' src={logo} alt='student'></img>
+            <p>Student</p>
+          </div>
+          <div className='tutor'>
+            <img className='logo' src={logo} alt='tutor' onClick={tutorInterface}></img>
+            <p>Tutor</p>
+          </div>
+        </div>
+        <div className='infoLine date'>
+          <h3 className='title'>Date: </h3>
+          <h3 className='text'>Mon 09/11/2924</h3>
+        </div>
     
-    <h1>Student's View</h1>
-    <button onClick={joinSession}>Join Session</button>
-    </>
-  )
-}
+        <div className='infoLine startTime'>
+          <h3 className='title'>Start Time: </h3>
+          <h3 className='text'>12am PCT</h3>
+        </div>
+    
+        <div className='infoLine endTime'>
+          <h3 className='title'>End Time: </h3>
+          <h3 className='text'>2am PCT</h3>
+        </div>
+    
+        <h4 id='message'>Session is ongoing</h4>
+        <div className='joinSession'>
+          <button onClick={startSession}>Join Session</button>
+        </div>
+        </>);
+    } else {
+      renderComponent = (<>
+        <div className='headTitle'>
+          <h1>Meeting 68757</h1>
+          <div className='student'>
+            <img className='logo' src={logo} alt='student'></img>
+            <p>Student</p>
+          </div>
+          <div className='tutor'>
+            <img className='logo' src={logo} alt='tutor' onClick={tutorInterface}></img>
+            <p>Tutor</p>
+          </div>
+        </div>
+        <div className='infoLine date'>
+          <h3 className='title'>Date: </h3>
+          <h3 className='text'>Mon 09/11/2924</h3>
+        </div>
+    
+        <div className='infoLine startTime'>
+          <h3 className='title'>Start Time: </h3>
+          <h3 className='text'>12am PCT</h3>
+        </div>
+    
+        <div className='joinSession'>
+          <button onClick={startSession}>Join Session</button>
+        </div>
+        </>)
+    
+    }
+  }
+    
+    return (
+      <div className='headTitle'>
+        <h1>Meeting 68757</h1>
+        <div className='student'>
+          <img className='logo' src={logo} alt='student' onClick={studentInterface}></img>
+          <p>Student</p>
+        </div>
+        <div className='tutor'>
+          <img className='logo' src={logo} alt='tutor' onClick={tutorInterface}></img>
+          <p>Tutor</p>
+        </div>
+      </div>
+    )
+
+  }
+
 
 export default App
