a5a426f2f4c589333ca63b82dfc478288325163d
IamLucifer0312 <117656374+IamLucifer0312@users.noreply.github.com>
2023-11-27 22:31:51 +0700

    finish fe prototype

--- client/app/src/App.jsx
+++ client/app/src/App.jsx
@@ -5,183 +5,29 @@ import { useNavigate } from "react-router-dom";
 import './App.css'
 import axios from 'axios';
 import logo from './images/address.png'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import StudentInterface from './studentInterface'
+import TutorInterface from './tutorInterface'
+
 
 const mySessionID = "123" // This is a fake ID, will have to implement scheduling feature
 
 
 function App() {
   // const navigate = useNavigate();
-  
-  const [inputUrl, setInputUrl] = useState("");
-  const [renderComponent, setRenderComponent] = useState(null);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StudentInterface />} />
+        <Route path="/student" element={<StudentInterface />} />
+        <Route path="/tutor" element={<TutorInterface />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
-  
-
-  const handleChange = (event) => {
-    setInputUrl(event.target.value)
-  }
-
-
-  const startSession = async () => {
-    // Set URL
-    let lastInputUrl = inputUrl;
-
-
-    setInputUrl("");
-    //  Send update request to backend
-    // PATCH({sID: '123', status: 'ongoing', liveShareUrl: inputUrl})
-    const response = await axios.patch(`http://localhost:3000/api/v1/session/${mySessionID}`, {
-      status: 'ongoing', 
-      liveShareUrl: lastInputUrl});
-      
-    console.log(response);
-   
-  }
-
-  // TUTOR
-  const tutorInterface = async () => {
-    setRenderComponent(
-      <>
-      <div className='headTitle'>
-        <h1>Meeting 68757</h1>
-        <div className='student'>
-          <img className='logo' src={logo} alt='student'></img>
-          <p>Student</p>
-        </div>
-        <div className='tutor'>
-          <img className='logo' src={logo} alt='tutor' onClick={tutorInterface}></img>
-          <p>Tutor</p>
-        </div>
-      </div>
-     
-  
-      <div className='infoLine date'>
-        <h3 className='title'>Date: </h3>
-        <h3 className='text'>Mon 09/11/2924</h3>
-      </div>
-  
-      <div className='infoLine startTime'>
-        <h3 className='title'>Start Time: </h3>
-        <h3 className='text'>12am PCT</h3>
-      </div>
-  
-      <div className='infoLine endTime'>
-        <h3 className='title'>End Time: </h3>
-        <h3 className='text'>2am PCT</h3>
-      </div>
-  
-      <h4 id='message'>To start the meeting, enter Live Share URL</h4>
-      <div className='startSession'>
-        <input type="text" placeholder='Live Share URL' onChange={handleChange}/>
-        <button onClick={startSession}>Start Session</button>
-      </div>
-      {/* <h1>Tutor's View</h1>
-      <input type="text" placeholder='Live Share URL' onChange={handleChange}/>
-      <button onClick={startSession}>Start Session</button>
-      <p>Live Share URL: {inputUrl}</p>
-      
-      <h1>Student's View</h1>
-      <button onClick={joinSession}>Join Session</button> */}
-      </>
-      )
-    }
-
-  
-
-  // STUDENT
-  const studentInterface = async () => {
-    // Only enable when time comes
-    // request session data from backend
-    
-    // GET({sID: '123'})
-    const response = await axios.get(`http://localhost:3000/api/v1/session/${mySessionID}`);
-    console.log(response.data);
-    
-    // If no URL yet then throw Error
-    if (!response.data.liveShareUrl) {
-      throw new Error('No URL yet');
-    }
-
-    if (response.data.status === 'ongoing') {
-      // Redirect to live share URL
-      setRenderComponent(<>
-        <div className='headTitle'>
-          <h1>Meeting 68757</h1>
-          <div className='student'>
-            <img className='logo' src={logo} alt='student'></img>
-            <p>Student</p>
-          </div>
-          <div className='tutor'>
-            <img className='logo' src={logo} alt='tutor' onClick={tutorInterface}></img>
-            <p>Tutor</p>
-          </div>
-        </div>
-        <div className='infoLine date'>
-          <h3 className='title'>Date: </h3>
-          <h3 className='text'>Mon 09/11/2924</h3>
-        </div>
-    
-        <div className='infoLine startTime'>
-          <h3 className='title'>Start Time: </h3>
-          <h3 className='text'>12am PCT</h3>
-        </div>
-    
-        <div className='infoLine endTime'>
-          <h3 className='title'>End Time: </h3>
-          <h3 className='text'>2am PCT</h3>
-        </div>
-    
-        <h4 id='message'>Session is ongoing</h4>
-        <div className='joinSession'>
-          <button onClick={startSession}>Join Session</button>
-        </div>
-        </>);
-    } else {
-      renderComponent = (<>
-        <div className='headTitle'>
-          <h1>Meeting 68757</h1>
-          <div className='student'>
-            <img className='logo' src={logo} alt='student'></img>
-            <p>Student</p>
-          </div>
-          <div className='tutor'>
-            <img className='logo' src={logo} alt='tutor' onClick={tutorInterface}></img>
-            <p>Tutor</p>
-          </div>
-        </div>
-        <div className='infoLine date'>
-          <h3 className='title'>Date: </h3>
-          <h3 className='text'>Mon 09/11/2924</h3>
-        </div>
-    
-        <div className='infoLine startTime'>
-          <h3 className='title'>Start Time: </h3>
-          <h3 className='text'>12am PCT</h3>
-        </div>
-    
-        <div className='joinSession'>
-          <button onClick={startSession}>Join Session</button>
-        </div>
-        </>)
-    
-    }
-  }
-    
-    return (
-      <div className='headTitle'>
-        <h1>Meeting 68757</h1>
-        <div className='student'>
-          <img className='logo' src={logo} alt='student' onClick={studentInterface}></img>
-          <p>Student</p>
-        </div>
-        <div className='tutor'>
-          <img className='logo' src={logo} alt='tutor' onClick={tutorInterface}></img>
-          <p>Tutor</p>
-        </div>
-      </div>
-    )
-
-  }
-
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
 
 export default App
