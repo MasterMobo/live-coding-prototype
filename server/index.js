dcadb5f1f903b5fa04eb4810a7216f25cc463777
MasterMobo <mastermobo101@gmail.com>
2023-11-27 18:45:55 +0700

    added socket.io to server

--- server/index.js
+++ server/index.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
 const cors = require("cors");
 
@@ -14,8 +19,18 @@ let session = {
   endTime: "huhu",
 };
 
+const userSocket = new Map();
+
 app.use(cors());
 app.use(express.json());
+
+io.on("connection", (socket) => {
+  console.log(`Client connected with Socket ID: ${socket.id}`);
+
+  const userId = socket.handshake.query.userId;
+  userSocket.set(userId, socket.id);
+  console.log(userSocket);
+});
 
 app.get("/api/v1", (req, res) => {
   res.json({ message: "Hello World" });
@@ -31,7 +46,8 @@ app.patch("/api/v1/session/:id", (req, res) => {
   const { status, liveShareUrl } = req.body;
   session.status = status;
   session.liveShareUrl = liveShareUrl;
+  io.emit("sessionStart", session);
   res.json(session);
 });
 
-app.listen(3000, console.log("Listening on port 3000"));
+server.listen(3000, console.log("Listening on port 3000"));
